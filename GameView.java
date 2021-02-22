e3a095c38526584b411de77cbd9e7695a6c5597a
lSAPFil <61491034+lSAPFil@users.noreply.github.com>
2021-02-22 18:06:53 +0300

    Add files via upload

--- GameView.java
+++ GameView.java
@@ -5,20 +5,23 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Handler;
-import android.os.SystemClock;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Timer;
 
-public class GameView extends View {
+public class GameView extends View implements OnClickListener {
     private Bitmap bmGrass1,bmGrass2, bmSnake, bmApple,bmWall;
     private int h=21, w=12;
     public static int sizeOfMap=75*Constanta.SCREEN_WIDH/1080;
@@ -28,17 +31,35 @@ public class GameView extends View {
     private float mx,my;
     private Handler handler;
     private Runnable r ;
-    private Runnable runnable ;
     public MainActivity main;
     private Apple apple;
     private boolean dead=false;
     public int valueApple=0;
-    private Timer timer;
+    public boolean guide2=false;
+    public RelativeLayout imaga;
+    public ImageView image1;
+    public ImageView image2;
+    public ImageView image3;
+    public ImageView image4;
+    public RelativeLayout imaga1;
     public long start = System.currentTimeMillis();
+    private SoundPool sounds;
+    private int die;
+    private int eat;
+    private int music;
 
-//    public int valueRecords;
-    public GameView(Context context, @Nullable AttributeSet attrs){ /////////Данный класс отрисовывается только один раз. Он не повторяется (на случай если ты будешь сомневаться)
+
+    public GameView(Context context, @Nullable AttributeSet attrs){
+
+
         super(context, attrs);
+
+        sounds = new SoundPool(1, AudioManager.STREAM_MUSIC,0);
+        die = sounds.load(context, R.raw.die, 1);
+        eat = sounds.load(context, R.raw.eat, 1);
+        music = sounds.load(context, R.raw.music, 1);
+
+
         bmGrass1= BitmapFactory.decodeResource(this.getResources(),R.drawable.grass);
         bmGrass1=Bitmap.createScaledBitmap(bmGrass1,sizeOfMap,sizeOfMap,true);
         bmGrass2= BitmapFactory.decodeResource(this.getResources(),R.drawable.grass03);
@@ -47,8 +68,6 @@ public class GameView extends View {
         bmSnake=Bitmap.createScaledBitmap(bmSnake,14*sizeOfMap,sizeOfMap,true);
         bmApple= BitmapFactory.decodeResource(this.getResources(),R.drawable.apple);
         bmApple=Bitmap.createScaledBitmap(bmApple,sizeOfMap,sizeOfMap,true);
-        bmWall=BitmapFactory.decodeResource(this.getResources(),R.drawable.wall);
-        bmWall=Bitmap.createScaledBitmap(bmWall,sizeOfMap,sizeOfMap,true);
         for(int i=0;i<(h+1);i++)
         {
             for(int j=0;j<(w+1);j++)
@@ -63,7 +82,7 @@ public class GameView extends View {
                 }
             }
         }
-        snake=new Snake(bmSnake,arrGrass.get(4).getX(),arrGrass.get(70).getY(),4);///285 285 (границы (на 286 прога вылетает по причине выхода за границы. Значт буду килить змейку на 0 и 285 клетке))
+        snake=new Snake(bmSnake,arrGrass.get(4).getX(),arrGrass.get(70).getY(),4);
         apple=new Apple(bmApple,arrGrass.get(randomApple()[0]).getX(),arrGrass.get(randomApple()[1]).getX());
         handler = new Handler();
         r=new Runnable() {
@@ -77,13 +96,11 @@ public class GameView extends View {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
          int a=event.getActionMasked();
-         switch (a){
-             case MotionEvent.ACTION_MOVE:{
+         switch (a){ case MotionEvent.ACTION_MOVE:{
                  if(move==false){
                      mx=event.getX();
                      my=event.getY();
-                     move=true;
-                 }else{
+                     move=true; }else{
                      if(mx-event.getX()>100*Constanta.SCREEN_WIDH/1080&&!snake.isMove_right()){
                          mx=event.getX();
                          my=event.getY();
@@ -99,92 +116,100 @@ public class GameView extends View {
                      }else if(event.getY()-my>100*Constanta.SCREEN_WIDH/1080&&!snake.isMove_top()){
                          mx=event.getX();
                          my=event.getY();
-                         snake.setMove_bottom(true);
-                     }
-
+                         snake.setMove_bottom(true); }
                  }
-                 break;
-             }
+                 break; }
              case MotionEvent.ACTION_UP:{
                  mx=0;
                  my=0;
                  move=false;
-                 break;
-             }
+                 break; }
          }
-        return true;
-    }
+        return true; }
 
     @Override
     public void draw(Canvas canvas){
-        if(canvas==null){
-            Log.d("start", "КОНЕЦ");
-            SystemClock.sleep(10000);
-        }
-
         super.draw(canvas);
-        timer = new Timer();
         canvas.drawColor(0xFF1B007C);
         for (int i=0; i<arrGrass.size();i++){
             canvas.drawBitmap(arrGrass.get(i).getBm(),arrGrass.get(i).getX(),arrGrass.get(i).getY(), null);
         }
+        image1.setOnClickListener(this);
+        image2.setOnClickListener(this);
+        image3.setOnClickListener(this);
+        image4.setOnClickListener(this);
+        imaga.setOnClickListener(this);
 
-        snake.update();
-        snake.draw(canvas);
-        apple.draw(canvas);
+        if(guide2==true){
+                snake.update();
+                snake.draw(canvas);
+                apple.draw(canvas);
 
-        if(snake.getArrPartSnake().get(0).getrBody().intersect(apple.getR())){
-            randomApple();
-            this.valueApple++;
-            apple.reset(arrGrass.get(randomApple()[0]).getX(),arrGrass.get(randomApple()[1]).getY());
-            main.appleValue.setText(Integer.toString(this.valueApple));
-            snake.addPart();
-            Log.d("start", "Яблоко скушано");
-            this.start= System.currentTimeMillis();
-
-        }else{
-            long finish = System.currentTimeMillis();
-            if(finish-this.start>=2000){
+            }
+            if(snake.getArrPartSnake().get(0).getrBody().intersect(apple.getR())){
+                if(dead==false){
+                    sounds.play(eat, 1, 1, 0, 0, 0);
+                }
+                randomApple();
+                this.valueApple++;
                 apple.reset(arrGrass.get(randomApple()[0]).getX(),arrGrass.get(randomApple()[1]).getY());
-                Log.d("start", "Рандом яблока");
+                main.appleValue.setText(Integer.toString(this.valueApple));
+                snake.addPart();
                 this.start= System.currentTimeMillis();
-            }
-        }
 
-        handler.postDelayed(r,100);
-
-
-        for (int i=1; i!=snake.getArrPartSnake().size();i++){
-            if (snake.getArrPartSnake().get(0).getrBody().intersect(snake.getArrPartSnake().get(i).getrBody())){/////
-                dead=true;
+            }else{
+                long finish = System.currentTimeMillis();
+                if(finish-this.start>=2000){
+                    apple.reset(arrGrass.get(randomApple()[0]).getX(),arrGrass.get(randomApple()[1]).getY());
+                    this.start= System.currentTimeMillis();
+                }
             }
-        }
+            handler.postDelayed(r,100);
+            for (int i=1; i!=snake.getArrPartSnake().size();i++){
+                if (snake.getArrPartSnake().get(0).getrBody().intersect(snake.getArrPartSnake().get(i).getrBody())){/////
+                    dead=true;
+                }
+            }
+            for(int i=0;i<=21;i++)
+            {
+                if(i<=12 &&snake.getArrPartSnake().get(0).getrBody().intersect(arrGrass.get(i).getR())){
+                    dead=true;
+                    sounds.play(die, 1, 1, 0, 0, 1);
 
-        for(int i=0;i<=21;i++)
-        {
-            if(i<=12 &&snake.getArrPartSnake().get(0).getrBody().intersect(arrGrass.get(i).getR())){
-                dead=true;
-            }
-            if(snake.getArrPartSnake().get(0).getrBody().intersect(arrGrass.get(i*13).getR())){
-                dead=true;
-            }
-            if(i>=1 && snake.getArrPartSnake().get(0).getrBody().intersect(arrGrass.get(i*13-1).getR())){
-                dead=true;
-            }
-            if(i<=12 && snake.getArrPartSnake().get(0).getrBody().intersect(arrGrass.get(272+i).getR())){
-                dead=true;
-            }
-        }
+                }
+                if(snake.getArrPartSnake().get(0).getrBody().intersect(arrGrass.get(i*13).getR())){
+                    dead=true;
+                    sounds.play(die, 1, 1, 0, 0, 1);
 
+                }
+                if(i>=1 && snake.getArrPartSnake().get(0).getrBody().intersect(arrGrass.get(i*13-1).getR())){
+                    dead=true;
+                    sounds.play(die, 1, 1, 0, 0, 1);
 
-        if (dead==true) {
-            Log.d("start", "Змейка умерла");
-            snake.setMove_top(false);
-            snake.setMove_right(false);
-            snake.setMove_left(false);
-            snake.setMove_bottom(false);
-            dead=false;////delete (Написала для того, чтобы дебагер потом не спамил мне каждый шаг напоминанием о том, что она уже сдохла)
-        }
+                }
+                if(i<=12 && snake.getArrPartSnake().get(0).getrBody().intersect(arrGrass.get(272+i).getR())){
+                    dead=true;
+                    sounds.play(die, 1, 1, 0, 0, 1);
+
+                }
+                if(dead==true){
+                    super.draw(canvas);
+                    canvas.drawColor(0xFF1B007C);
+                    for (int j=0; j<arrGrass.size();j++){
+                        canvas.drawBitmap(arrGrass.get(j).getBm(),arrGrass.get(j).getX(),arrGrass.get(j).getY(), null);
+                    }
+                    main.valueRecord=valueApple;
+                    main.recordValue.setText(Integer.toString(main.valueRecord));
+                    main.newRecordValue.setText(Integer.toString(main.valueRecord));
+                    Log.d("start"," "+main.valueRecord+" "+main.record);
+
+                    imaga1.setVisibility(View.VISIBLE);
+                    if(move==true){
+                        imaga1.setVisibility(View.INVISIBLE);
+                        System.exit(0);
+                    }
+                }
+            }
     }
 
     public <valueApple> int[] randomApple(){
@@ -209,5 +234,33 @@ public class GameView extends View {
         }
         return xy;
     }
+
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.helpPage1:
+                image1.setVisibility(View.INVISIBLE);
+                image2.setVisibility(View.VISIBLE);
+                break;
+            case R.id.helpPage2:
+                image2.setVisibility(View.INVISIBLE);
+                image3.setVisibility(View.VISIBLE);
+                break;
+            case R.id.helpPage3:
+                image3.setVisibility(View.INVISIBLE);
+                image4.setVisibility(View.VISIBLE);
+                break;
+            case R.id.helpPage4:
+                image4.setVisibility(View.INVISIBLE);
+                imaga.setVisibility(View.VISIBLE);
+                break;
+            case R.id.swipe:
+                imaga.setVisibility(View.INVISIBLE);
+                this.guide2 = true;
+                break;
+        }
+    }
+
 
 }
--- MainActivity.java
+++ MainActivity.java
@@ -1,19 +1,54 @@
 package com.example.snake20;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class MainActivity extends AppCompatActivity {
     public static int valueApple = 0 ;
-//    public static int valueRecord = 0 ;
+    public static int valueRecord = 0 ;
     public TextView appleValue;
-//    public TextView recordValue;
+    public TextView recordValue;
+    public TextView newRecordValue;
+    public  String contents;
+
+    private SharedPreferences json;
+
+    private static final String PREF_FILE = "RECORDS"; // file name
+    private static final String PREF_RECORD = "Value"; // tag in file
+
+    public int record;
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        json = getSharedPreferences(PREF_FILE, MODE_PRIVATE); // load file
+//        record = json.getInt(PREF_FILE, 0);
+//
+//        //game stops
+//        if (valueRecord > record) {
+//            SharedPreferences.Editor edit = json.edit();
+//            edit.putInt(PREF_RECORD, valueRecord);
+//            edit.apply();
+//        }
+//
         DisplayMetrics dm = new DisplayMetrics();
         this.getWindowManager().getDefaultDisplay().getMetrics(dm);
         Constanta.SCREEN_WIDH=dm.widthPixels;
@@ -21,15 +56,240 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         GameView g= findViewById(R.id.gv);
         appleValue=findViewById(R.id.txt_score);
-//        recordValue=findViewById(R.id.txt_best_score);
-        g.main = this;
-//           Log.d("start", "HELLO WORLD");
+         recordValue=findViewById(R.id.txt_best_score_final);
+        newRecordValue=findViewById(R.id.txt_best_score);
 
+//        String file="value.txt";
+//        try {
+//            FileOutputStream fOut = openFileOutput(file, MODE_WORLD_READABLE);
+//            fOut.write(Integer.toString(valueRecord).getBytes());
+//            fOut.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+
+
+//        try {
+//            FileInputStream fin = openFileInput(file);
+//            int c;
+//            recordString="";
+//            while ((recordString=fin.read())!=1){
+//                recordString=recordString+Character.toString(recordString);
+//            }
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+//        if (Files.exists(Paths.get("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\raw\\value.txt"))) {
+//            Path recordFile = Paths.get("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\raw\\value.txt");
+//
+//            List<String> recordslist = null;
+//            try {
+//                recordslist = Files.readAllLines(recordFile);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            recordStringOld = recordslist.get(recordslist.size()-1);
+//            int recordIntOld = Integer.parseInt(recordStringOld);
+//            if (valueRecord > recordIntOld) {
+//                try {
+//                    Files.delete(recordFile);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                try {
+//                    recordPathNew = Files.createFile(Paths.get("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\raw\\value.txt"));
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                recordPathNew = Paths.get("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\raw\\value.txt");
+//                recordStringOld = Integer.toString(valueRecord);
+//                byte[] bytes = recordStringOld.getBytes();
+//                try {
+//                    Files.write(Paths.get("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\raw\\value.txt"),bytes);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//
+//        else if (!Files.exists(Paths.get("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\raw\\value.txt"))) {
+//            try {
+//                recordPathNew = Files.createFile(Paths.get("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\raw\\value.txt"));
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            recordPathNew = Paths.get("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\raw\\value.txt");
+//            recordStringOld = Integer.toString(valueRecord);
+//            byte[] bytes = recordStringOld.getBytes();
+//            try {
+//                Files.write(Paths.get("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\raw\\value.txt"),bytes);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//        }
+
+
+
+
+
+//        try {
+//            contents = readUsingFiles("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\raw\\value.txt");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        File myF = new File("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\raw\\value.txt");
+//        try {
+//            FileOutputStream foo = new FileOutputStream(myF,false);
+//            byte[] myB= "0".getBytes();
+//            foo.write(myB);
+//            foo.close();
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+
+
+
+//        if(Integer.valueOf(recordString)<valueRecord){
+//            writeFile(this,"value.txt",Integer.toString(valueRecord));
+//        }
         appleValue.setText(Integer.toString(valueApple));
-//        recordValue.setText(Integer.toString(valueRecord));
+        recordValue.setText(Integer.toString(valueRecord));
+        newRecordValue.setText(Integer.toString(valueRecord));
 
+
+
+//        File file = new File("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\raw\\value");
+//        InputStream in=null;
+//        try {
+//            in= new BufferedInputStream(new FileInputStream(file));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if(in!=null){
+//                try {
+//                    in.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//        Scanner scan = new Scanner(in);
+//        line= scan.hasNext() ? scan.next():"";
+
+
+
+        //        File file= new File("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\raw\\value");
+//        try {
+//            FileInputStream input = new FileInputStream(file);
+//
+//            while (input.available()>0){
+//                buf=input.read();
+//            }
+//            input.close();
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+
+
+//        String line;
+//        recordValue.setText(Integer.toString(buf));
+
+
+        g.imaga=findViewById(R.id.swipe);
+        g.imaga1=findViewById(R.id.bd_dialog);
+        g.image1=findViewById(R.id.helpPage1);
+        g.image2=findViewById(R.id.helpPage2);
+        g.image3=findViewById(R.id.helpPage3);
+        g.image4=findViewById(R.id.helpPage4);
+        g.main = this;
+
+
+//        try {
+//            Scanner scan = new Scanner(new File("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\text-file\\value"));
+//            while (scan.hasNext()){
+//                line=scan.next();
+//                buf=Integer.valueOf(line);
+//                if(buf>valueRecord){
+//                    try {
+//                        FileWriter file = new FileWriter("C:\\Users\\sapfi\\AndroidStudioProjects\\Snake20\\app\\src\\main\\res\\text-file\\value");
+//                        file.write(line);
+//                        file.close();
+//                    }catch (IOException ex){
+//                        System.out.println(ex.getMessage());
+//                    }
+//                    recordValue.setText(line);
+//                    newRecordValue.setText(line);
+//                }
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
+
+
+        startService(new Intent(this, MyService.class));
 
     }
+    public void onBackPressed() {
+        stopService(new Intent(this, MyService.class));
+    }
+
+
+    private String readFile(Context context){
+        String myValue="";
+        String fileName="value.txt";
+        File dir = context.getFilesDir();
+        File file = new File(dir,fileName);
+        try{
+            FileInputStream fis=new FileInputStream(file);
+            BufferedReader buf=new BufferedReader(new InputStreamReader(fis));
+            String line;
+            while ((line=buf.readLine())!=null){
+                myValue=myValue+line;
+            }
+            buf.close();
+            fis.close();
+
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        return myValue;
+    }
+
+    private void writeFile(Context context, String file, String body){
+        File dir = new File(context.getFilesDir(),"mydir");
+        if(!dir.exists()){
+            dir.mkdir();
+        }
+        try {
+            File gfile=new File(dir,file);
+            FileWriter writer = new FileWriter(gfile);
+            writer.append(body);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+     private static String readUsingFiles(String file) throws IOException{
+        return new String(Files.readAllBytes(Paths.get(file)));
+     }
+
 
 }
 
